e61f229eb7b598611c941011c67992820d50a064
ahsan-bpo <igotpowerwash.dev@gmail.com>
2025-03-21 22:19:38 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar/Navbar";
+import HeroSection from "./components/Hero/hero";
 
 export default function Home() {
   return (
    <div>
     <Navbar/>
+    <HeroSection/>
    </div>
   );
 }
